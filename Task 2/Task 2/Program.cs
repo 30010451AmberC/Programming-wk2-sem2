7a8c2894e4c60ab436e2f62ced259a9c248a31c8
Amber Cook <30010451@student.toiohomai.ac.nz>
2019-03-07 10:41:34 +1300

    Almost complete. 1 last task to do

--- Task 2/Task 2/Program.cs
+++ Task 2/Task 2/Program.cs
@@ -12,11 +12,41 @@ namespace Task_2
         {
             //Create new employee array to hold 5 (employee) objects
             Employee[] workers = new Employee[5];
+            
+
+            //Loop to collect data of 5 employees to fill workers array
+            for(int i = 0; i < workers.Length; i++)
+            {
+
+                //Collecting user data and storing data in variables
+                Console.WriteLine("Please enter name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("enter phone number: ");
+                int phoneNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter email: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("enter IRD number: ");
+                int irdNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter bank number: ");
+                int bankNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter Username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("Please Password: ");
+                string pwd = Console.ReadLine();
+              
+                //Created new Employee object and passed the data collected from user to constructor
+                Employee e1 = new Employee(name, phoneNumber, email, irdNum, bankNum, username, pwd);
+
+                //Adding the new employee object to the array.
+                workers[i] = e1;
+            }
+            //Write back to console data recorded.
         }
     }
 
     class Employee
         {
+        //Created properties for employee class
         private string name;
         private int phoneNumber;
         private string email;
@@ -25,6 +55,7 @@ namespace Task_2
         private string username;
         private string pwd;
 
+        //Created getters & setters for each property
         public string Name { get { return name; } set { value = name; } }
         public int PhoneNumber { get { return phoneNumber; } set { value = phoneNumber; } }
         public string Email { get { return email; } set { value = email; } }
@@ -32,6 +63,18 @@ namespace Task_2
         public int BankNum { private get { return bankNum; } set { value = bankNum; } }
         public string Username { get { return username; } set { value = username; } }
         public string Pwd { get { return pwd; } set { value = pwd; } }
+
+        //Created a constructor to write to the console when a object has been created
+        public Employee(string _name, int _phoneNumber, string _email, int _irdNum, int _bankNum, string _username, string _pwd)
+        {
+            Name = _name;
+            PhoneNumber = _phoneNumber;
+            Email = _email;
+            IrdNum = _irdNum;
+            BankNum = _bankNum;
+            Username = _username;
+            Pwd = _pwd;
+        }
 
     }
 }
