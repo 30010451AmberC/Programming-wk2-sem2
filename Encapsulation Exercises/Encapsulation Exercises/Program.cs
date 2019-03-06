07bd20dc3551fdfa60e4ff024e1966d77566b710
Amber Cook <30010451@student.toiohomai.ac.nz>
2019-03-06 16:20:56 +1300

    task 2 not complete

---
+++ Encapsulation Exercises/Encapsulation Exercises/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK 1:
+
+            //Created new Card object 'c1'
+            Card c1 = new Card();
+
+            //Ask user to input data for c1 object
+            Console.WriteLine("Please enter Cardholders name: ");
+            c1.Name = Console.ReadLine();
+            Console.WriteLine("Please enter Card Number: ");
+            c1.CardNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Expiry date: ");
+            c1.Expiry = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Security Code: ");
+            c1.SecurityCode = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Cardholders Name: {c1.Name}\nExpiry Date: {c1.Expiry}");
+        }
+    }
+
+    class Card
+    {
+        //Properties of Card class
+        private string name;
+        private int cardNum;
+        private int expiry;
+        private int securityCode;
+
+        //Attributes
+        public string Name { get { return name; } set { name = value; } }
+        public int CardNum {private get { return cardNum; } set { cardNum = value; } }
+        public int Expiry { get { return expiry; } set { expiry = value; } }
+        public int SecurityCode { private get { return securityCode; } set { securityCode = value; } }
+
+        //Constructor
+        public Card()
+            {
+            Console.Write("A new cardholder has been created.");
+            }
+    }
+}
---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new employee array to hold 5 (employee) objects
+            Employee[] workers = new Employee[5];
+        }
+    }
+
+    class Employee
+        {
+        private string name;
+        private int phoneNumber;
+        private string email;
+        private int irdNum;
+        private int bankNum;
+        private string username;
+        private string pwd;
+
+        public string Name { get { return name; } set { value = name; } }
+        public int PhoneNumber { get { return phoneNumber; } set { value = phoneNumber; } }
+        public string Email { get { return email; } set { value = email; } }
+        public int IrdNum { private get { return irdNum; } set { value = irdNum; } }
+        public int BankNum { private get { return bankNum; } set { value = bankNum; } }
+        public string Username { get { return username; } set { value = username; } }
+        public string Pwd { get { return pwd; } set { value = pwd; } }
+
+    }
+}
