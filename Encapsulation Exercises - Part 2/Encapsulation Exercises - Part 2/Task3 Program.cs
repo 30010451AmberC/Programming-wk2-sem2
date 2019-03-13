2abdfe808ef4acd34b3eb87e898c15c87978fd96
Amber Cook <30010451@student.toiohomai.ac.nz>
2019-03-14 10:28:06 +1300

    task3 not complete

--- Encapsulation Exercises - Part 2/Encapsulation Exercises - Part 2/Task3 Program.cs
+++ Encapsulation Exercises - Part 2/Encapsulation Exercises - Part 2/Task3 Program.cs
@@ -10,7 +10,40 @@ namespace Encapsulation_Exercises___Part_2
     {
         static void Main(string[] args)
         {
+            //Created Customer object
+            Customer c1 = new Customer();
 
+            //Prompting user for input
+            Console.WriteLine("Please enter name: ");
+            string name = Console.ReadLine();
+            CheckString(name);
+            Console.WriteLine("Complete.");
+            Console.WriteLine("enter username: ");
+            string username = Console.ReadLine();
+            CheckString(username);
+            Console.WriteLine("Complete!");
+            Console.WriteLine("Enter password: ");
+            string pwd = Console.ReadLine();
+            CheckString(pwd);
+            Console.WriteLine("Complete!");
+            Console.WriteLine("Enter payment method: ");
+            string payMethod = Console.ReadLine();
+            CheckString(payMethod);
+            Console.WriteLine("Complete!");
+        }
+
+        //Method to check if user input is valid
+        static string CheckString(string input)
+        {
+            do
+            {
+                if (input == "" || input == null)
+                {
+                    Console.WriteLine("Please enter customer name correctly");
+                    input = Console.ReadLine();
+                }
+            } while (input == "");
+            return input;
         }
     }
 
@@ -22,14 +55,14 @@ namespace Encapsulation_Exercises___Part_2
         private string username;
         private string pwd;
         private string payMethod;
-        private string shopCart;
+        string[] shopCart = new string[4];
 
         //Customer properties getters and setters
         public string Name { get { return name; } set { name = value; } }
         public string Username { get { return username; } set { username = value; } }
         public string Pwd { get { return pwd; } set { pwd = value; } }
         public string PayMethod { get { return payMethod; } set { payMethod = value; } }
-        public string ShopCart { get { return shopCart; } set { shopCart = value; } }
+        public string[] ShopCart { get { return shopCart; } set { shopCart = value; } }
     }
 
     //Created Cart class
